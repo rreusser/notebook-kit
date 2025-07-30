dcac184763171f4b9f9f077a0f48da5f66206a63
Mike Bostock <mbostock@gmail.com>
2025-07-30 16:34:49 -0400

    extract a couple globals

--- bin/download.ts
+++ bin/download.ts
@@ -5,6 +5,9 @@ import {JSDOM} from "jsdom";
 import {toNotebook} from "../src/lib/notebook.js";
 import {serialize} from "../src/lib/serialize.js";
 
+const UI_ORIGIN = "https://observablehq.com";
+const API_ORIGIN = "https://api.observablehq.com";
+
 if (process.argv[1] === import.meta.filename) run();
 
 export default async function run(args?: string[]) {
@@ -29,16 +32,16 @@ export default async function run(args?: string[]) {
     return;
   }
 
-  const document = new JSDOM().window.document;
+  const {window} = new JSDOM();
   for (const positional of positionals) {
-    let url = new URL(positional, "https://observablehq.com");
-    if (url.origin === "https://observablehq.com") {
-      url = new URL(`/document${url.pathname.replace(/^\/d\//, "/")}`, "https://api.observablehq.com");
+    let url = new URL(positional, UI_ORIGIN);
+    if (url.origin === UI_ORIGIN) {
+      url = new URL(`/document${url.pathname.replace(/^\/d\//, "/")}`, API_ORIGIN);
     }
     const response = await fetch(url);
     if (!response.ok) throw new Error(`unable to fetch: ${url}`);
     const {title, nodes} = await response.json();
     for (const node of nodes) if (node.mode === "js") node.mode = "ojs";
-    process.stdout.write(serialize(toNotebook({title, cells: nodes}), {document}));
+    process.stdout.write(serialize(toNotebook({title, cells: nodes}), {document: window.document}));
   }
 }
